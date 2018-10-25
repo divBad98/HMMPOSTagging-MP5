72e35638622e581e51a8c288682bbfeed34e1633
divBad98 <db98uiuc@gmail.com>
2018-10-24 21:44:49 -0500

    first commit

---
+++ viterbi.py
@@ -0,0 +1,36 @@
+# viterbi.py
+# ---------------
+# Licensing Information:  You are free to use or extend this projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to the University of Illinois at Urbana-Champaign
+#
+# Created by Renxuan Wang (renxuan2@illinois.edu) on 10/18/2018
+
+"""
+This is the main entry point for MP5. You should only modify code
+within this file -- the unrevised staff files will be used for all other
+files and classes when code is run, so be careful to not modify anything else.
+"""
+
+'''
+TODO: implement the baseline algorithm.
+input:  training data (list of sentences, with tags on the words)
+        test data (list of sentences, no tags on the words)
+output: list of sentences, each sentence is a list of (word,tag) pairs. 
+        E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
+'''
+def baseline(train, test):
+    predicts = []
+    return predicts
+
+'''
+TODO: implement the Viterbi algorithm.
+input:  training data (list of sentences, with tags on the words)
+        test data (list of sentences, no tags on the words)
+output: list of sentences with tags on the words
+        E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
+'''
+def viterbi(train, test):
+    predicts = []
+    return predicts
